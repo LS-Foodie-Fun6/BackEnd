eaa8df7a17f77ab4d0f09382d5338e4db4d09bde
Daniel <danstad2012@gmail.com>
2020-01-09 09:39:32 -0800

    401 to 201

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -11,20 +11,10 @@ function restricted (req,res,next) {
   if(req.session && req.session.user) next()
   else {
     console.log('restricted')
-    res.status(401).json({message: 'go away, you dont belong here'})
+    res.status(201).json({message: 'go away, you dont belong here'})
     next()
   }
 }
-
-// function restricted(req, res, next) {
-//   console.log(req.headers.authorization)
-//   //console.log(req.session)
-//   if (req.session && req.session.user) next();
-//   else {
-//     console.log('restricted')
-//     res.status(401).json({ message: "You shall not pass!!" });
-//   }
-// };
 
 function authenticate(req, res, next) {
   const {Authorization} = req.headers
@@ -40,19 +30,3 @@ function authenticate(req, res, next) {
   }
   else res.status(400).json({message:'come back after you log in'})
 }
-
-
-// function authenticate(req, res, next) {
-//   const { authorization } = req.headers;
-//   if (authorization) {
-//     const secret = process.env.JWT_SECRET || "is it secret, is it safe?";
-//     jwt.verify(authorization, secret, function(err, decodedToken) {
-//       if (err) res.status(401).json({ message: "Invalid Token" }) 
-//       else {
-//         req.token = decodedToken;
-//         next();
-//       }
-//     });
-//   } 
-//   else res.status(400).json({ message: "Please login and try again" })
-// };
